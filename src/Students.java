9c8bf5b115f761df48fd7690e3a23494ee01fc81
juliamanaenko <julia.manaenko@gmail.com>
2016-12-06 17:51:54 +0200

    Julia's commit in Students.java

--- src/Students.java
+++ src/Students.java
@@ -2,6 +2,9 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 
 
 public class Students implements Comparable<Students> {
@@ -11,6 +14,40 @@ public class Students implements Comparable<Students> {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
+	private String surname;
+	private int course;
+	private int age;
+	
+	@JsonCreator
+	public void Parsing(@JsonProperty("surname")String name, @JsonProperty("course")int course, @JsonProperty("age")int age){
+		this.surname = surname;
+		this.course = course;
+		this.age = age;
+	}
+	
+	public String getSurname(){
+		return surname;
+	}
+	public void setSurname(String name){
+		this.surname = name;
+	}
+	
+	public int getCourse(){
+		return course;
+	}
+	public void setCourse(int course){
+		this.course = course;
+	}
+	
+	public int getAge(){
+		return age;
+	}
+	public void setAge(int age){
+		this.age = age;
+	}
+
+	
 	
 	}
 
